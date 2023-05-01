0a2e9cec75aa6b7c4ccbe6d026a33abf056d3b3b
Sander Koopmans <sk@Sanders-MacBook-Pro.local>
2023-05-01 16:09:33 +0200

    feat(mobileMenu): Outline mobile menu burger

--- app/components/header/Header.tsx
+++ app/components/header/Header.tsx
@@ -1,6 +1,7 @@
 import { Link } from "@remix-run/react";
 import { H1, H3 } from "../typography/Typography";
 import { Instagram, Linkedin, Mail, Menu, Search } from "lucide-react";
+import { MobileMenuButton } from "../mobileMenuButton/MobileMenuButton";
 
 export default function Header({ titles }) {
   return (
@@ -8,7 +9,8 @@ export default function Header({ titles }) {
       <aside className="row-span-1 w-full border-b-2 border-b-gray-300 px-4 py-2 lg:col-start-2 lg:col-end-2 lg:row-start-1 lg:row-end-1 lg:border-b-0 lg:border-l-2 lg:border-l-gray-300 lg:py-0 lg:px-2 lg:pt-8">
         <div className="flex h-full justify-between">
           <div className="block lg:hidden">
-            <Menu />
+            {/* <Menu /> */}
+            <MobileMenuButton />
           </div>
           <div className="flex gap-3 lg:h-full lg:flex-col lg:border-b-2 lg:border-b-gray-300">
             <a
