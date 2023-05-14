e71ad0d4802ab63c26e12612eccc8affc6937848
Sander Koopmans <sk@Sanders-MacBook-Pro.local>
2023-05-14 13:54:50 +0200

    feat(nav): Update links to show the right active link

--- app/routes/articles/index.tsx
+++ app/routes/articles/index.tsx
@@ -38,9 +38,12 @@ export default function Article() {
   const posts = useLoaderData();
 
   return (
-    <ClientOnly fallback={<h1 className="h-[600px]">Loading</h1>}>
-    {/* <ClientOnly fallback={null}> */}
-      {() => <ArticleGrid articles={posts} />}
-    </ClientOnly>
+    <>
+      <ClientOnly fallback={<h1 className="h-[600px]">Loading</h1>}>
+        {/* <ClientOnly fallback={null}> */}
+        {() => <ArticleGrid articles={posts} />}
+      </ClientOnly>
+      {/* <Outlet /> */}
+    </>
   );
 }
