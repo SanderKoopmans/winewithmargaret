954465cef8d18c96009f4c78840c7b8a437276c5
Sander Koopmans <sk@Sanders-MacBook-Pro.local>
2023-06-09 22:00:22 +0200

    feat(slider): Save progress, work needed on spacing and animation

--- app/blocks/slider/index.tsx
+++ app/blocks/slider/index.tsx
@@ -2,9 +2,9 @@ import { Paragraph } from "~/components/typography/Typography";
 
 export const Slider = ({ data }) => {
   console.log("Content: ", data);
-  return (<div className="w-full overflow-hidden">
-    <ul className="flex relative w-full">
-      {data.images.data.map(img => <li key={img.attributes.name} className="w-[80%]"><img src={`http://localhost:1337${img.attributes.url}`} alt={img.attributes.alternativeText} /></li>)}
+  return (<div className="overflow-hidden relative min-h-[500px] w-[900px] flex mx-auto">
+    <ul className="flex mx-auto basis-[700px]">
+      {data.images.data.map(img => <li key={img.attributes.name} className="relative max-w-[700px] grow-0 shrink-0 basis-full"><img src={`http://localhost:1337${img.attributes.url}`} alt={img.attributes.alternativeText} className="absolute max-w-[700px]"/></li>)}
     </ul>
   </div>)
 };
